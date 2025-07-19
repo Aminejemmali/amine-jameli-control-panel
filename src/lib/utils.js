207072a9a43fd55e7934045a41911a5a713a0181
Aminejemmali <jemmaliamine2000@gmail.com>
2025-07-19 15:12:57 +0100

    v0.0.1+1

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -9,7 +9,7 @@ export function cn(...inputs) {
 export const formatCurrency = (amount) => {
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
-    currency: 'USD',
+    currency: 'TND',
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
   }).format(amount);
